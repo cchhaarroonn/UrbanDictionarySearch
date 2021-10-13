937424f699278d363c0a5e56d2e944e11d2b39de
カロン <90192366+cchhaarroonn@users.noreply.github.com>
2021-10-13 09:28:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,17 @@ term = str(input("Please enter the term: "))
 
 url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define" #URL of API
 
-#Saveing input string into querystring variable so we can pass query that we want to search
+#Makeing query that we want to search
 querystring = {"term":str(term)}
 
-#Our headers for API request
 headers = {
     'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com",
-    'x-rapidapi-key': "5c42c03828mshc340236c62604fep162f35jsnafb5fcd1ae4a" #This is my API key from my account that i created on rapidapi, if you want you can create your own!
+    'x-rapidapi-key': "5c42c03828mshc340236c62604fep162f35jsnafb5fcd1ae4a" #You can use your own API key
     }
 
-#Sends API request to URL specified on top of code with headers and query string
 response = requests.request("GET", url, headers=headers, params=querystring)
 
-#This part asks person should repsponse be saved in a file or not, if not response will be printed in console
+#Save data to file part
 saveIntoFile = str(input("Do you want to save response into text file(y/n): "))
 
 if saveIntoFile == "y":
